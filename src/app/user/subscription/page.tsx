bd9e228d8279a35cf71c8881e1449bc572b9a326
imzyb <vanness.x@gmail.com>
2025-07-19 00:35:12 +0800

    创建

---
+++ src/app/user/subscription/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import React from 'react';
+
+export default function UserSubscriptionPage() {
+  return (
+    <div>
+      <h1>我的订阅</h1>
+      <p>这里将显示用户的订阅信息。</p>
+    </div>
+  );
+}
