c31a570d79c897d6d6291649565294bbaa7130c3
imzyb <vanness.x@gmail.com>
2025-07-19 01:12:55 +0800

    创建

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   distDir: "dist",
+  modularizeImports: {
+    'antd': {
+      transform: 'antd/lib/{{member}}',
+      skipDefaultConversion: true,
+    },
+  },
   /* config options here */
 };
 
