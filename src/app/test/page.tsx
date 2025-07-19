3a2775ffdc55da762fcbc1a390e42197b48e48ec
imzyb <vanness.x@gmail.com>
2025-07-19 12:39:18 +0800

    纠错

---
+++ src/app/test/page.tsx
@@ -0,0 +1,10 @@
+// src/app/test/page.tsx
+
+export default function TestPage() {
+  return (
+    <div>
+      <h1>Test Page Works</h1>
+      <p>If you can see this, the basic Next.js routing on Cloudflare Pages is working.</p>
+    </div>
+  );
+}
