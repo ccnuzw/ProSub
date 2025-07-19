2703607c6cfe1900ab1f89accc5c3c18934f5b00
imzyb <vanness.x@gmail.com>
2025-07-19 12:27:04 +0800

    纠错

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
-  distDir: "dist",
-  /* config options here */
 };
 
 export default nextConfig;
