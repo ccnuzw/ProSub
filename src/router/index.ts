163790cedf13c960f9a5be93518753d00e8610b1
imzyb <vanness.x@gmail.com>
2025-08-02 09:54:08 +0800

    修复路由配置，确保认证页面使用ClientLayout布局

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,121 +1,127 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import ClientLayout from '../components/ClientLayout.vue'
+
 const routes = [
   {
     path: '/',
-    redirect: '/dashboard'
-  },
-  {
-    path: '/dashboard',
-    name: 'Dashboard',
-    component: () => import('../views/Dashboard.vue'),
-    meta: { requiresAuth: true }
-  },
-  {
-    path: '/nodes',
-    name: 'Nodes',
-    component: () => import('../views/Nodes.vue'),
-    meta: { requiresAuth: true }
-  },
-  {
-    path: '/node-groups',
-    name: 'NodeGroups',
-    component: () => import('../views/NodeGroups.vue'),
-    meta: { requiresAuth: true }
-  },
-  {
-    path: '/profiles',
-    name: 'Profiles',
-    component: () => import('../views/Profiles.vue'),
-    meta: { requiresAuth: true }
-  },
-  {
-    path: '/subscriptions',
-    name: 'Subscriptions',
-    component: () => import('../views/Subscriptions.vue'),
-    meta: { requiresAuth: true }
-  },
-  {
-    path: '/rule-sets',
-    name: 'RuleSets',
-    component: () => import('../views/RuleSets.vue'),
-    meta: { requiresAuth: true }
+    redirect: '/dashboard',
+    component: ClientLayout,
+    meta: { requiresAuth: true },
+    children: [
+      {
+        path: '/dashboard',
+        name: 'Dashboard',
+        component: () => import('../views/Dashboard.vue'),
+        meta: { requiresAuth: true }
+      },
+      {
+        path: '/nodes',
+        name: 'Nodes',
+        component: () => import('../views/Nodes.vue'),
+        meta: { requiresAuth: true }
+      },
+      {
+        path: '/node-groups',
+        name: 'NodeGroups',
+        component: () => import('../views/NodeGroups.vue'),
+        meta: { requiresAuth: true }
+      },
+      {
+        path: '/profiles',
+        name: 'Profiles',
+        component: () => import('../views/Profiles.vue'),
+        meta: { requiresAuth: true }
+      },
+      {
+        path: '/subscriptions',
+        name: 'Subscriptions',
+        component: () => import('../views/Subscriptions.vue'),
+        meta: { requiresAuth: true }
+      },
+      {
+        path: '/rule-sets',
+        name: 'RuleSets',
+        component: () => import('../views/RuleSets.vue'),
+        meta: { requiresAuth: true }
+      },
+      {
+        path: '/user/change-password',
+        name: 'UserChangePassword',
+        component: () => import('../views/UserChangePassword.vue'),
+        meta: { requiresAuth: true }
+      },
+      {
+        path: '/user/profile',
+        name: 'UserProfile',
+        component: () => import('../views/UserProfile.vue'),
+        meta: { requiresAuth: true }
+      },
+      {
+        path: '/nodes/:id',
+        name: 'NodeEdit',
+        component: () => import('../views/NodeEdit.vue'),
+        meta: { requiresAuth: true }
+      },
+      {
+        path: '/nodes/new',
+        name: 'NodeNew',
+        component: () => import('../views/NodeNew.vue'),
+        meta: { requiresAuth: true }
+      },
+      {
+        path: '/profiles/:id',
+        name: 'ProfileEdit',
+        component: () => import('../views/ProfileEdit.vue'),
+        meta: { requiresAuth: true }
+      },
+      {
+        path: '/profiles/new',
+        name: 'ProfileNew',
+        component: () => import('../views/ProfileNew.vue'),
+        meta: { requiresAuth: true }
+      },
+      {
+        path: '/subscriptions/:id',
+        name: 'SubscriptionEdit',
+        component: () => import('../views/SubscriptionEdit.vue'),
+        meta: { requiresAuth: true }
+      },
+      {
+        path: '/subscriptions/new',
+        name: 'SubscriptionNew',
+        component: () => import('../views/SubscriptionNew.vue'),
+        meta: { requiresAuth: true }
+      },
+      {
+        path: '/node-groups/:id',
+        name: 'NodeGroupEdit',
+        component: () => import('../views/NodeGroupEdit.vue'),
+        meta: { requiresAuth: true }
+      },
+      {
+        path: '/node-groups/new',
+        name: 'NodeGroupNew',
+        component: () => import('../views/NodeGroupNew.vue'),
+        meta: { requiresAuth: true }
+      },
+      {
+        path: '/rule-sets/:id',
+        name: 'RuleSetEdit',
+        component: () => import('../views/RuleSetEdit.vue'),
+        meta: { requiresAuth: true }
+      },
+      {
+        path: '/rule-sets/new',
+        name: 'RuleSetNew',
+        component: () => import('../views/RuleSetNew.vue'),
+        meta: { requiresAuth: true }
+      },
+    ]
   },
   {
     path: '/user/login',
     name: 'Login',
     component: () => import('../views/Login.vue'),
-  },
-  {
-    path: '/user/change-password',
-    name: 'UserChangePassword',
-    component: () => import('../views/UserChangePassword.vue'),
-    meta: { requiresAuth: true }
-  },
-  {
-    path: '/user/profile',
-    name: 'UserProfile',
-    component: () => import('../views/UserProfile.vue'),
-    meta: { requiresAuth: true }
-  },
-  {
-    path: '/nodes/:id',
-    name: 'NodeEdit',
-    component: () => import('../views/NodeEdit.vue'),
-    meta: { requiresAuth: true }
-  },
-  {
-    path: '/nodes/new',
-    name: 'NodeNew',
-    component: () => import('../views/NodeNew.vue'),
-    meta: { requiresAuth: true }
-  },
-  {
-    path: '/profiles/:id',
-    name: 'ProfileEdit',
-    component: () => import('../views/ProfileEdit.vue'),
-    meta: { requiresAuth: true }
-  },
-  {
-    path: '/profiles/new',
-    name: 'ProfileNew',
-    component: () => import('../views/ProfileNew.vue'),
-    meta: { requiresAuth: true }
-  },
-  {
-    path: '/subscriptions/:id',
-    name: 'SubscriptionEdit',
-    component: () => import('../views/SubscriptionEdit.vue'),
-    meta: { requiresAuth: true }
-  },
-  {
-    path: '/subscriptions/new',
-    name: 'SubscriptionNew',
-    component: () => import('../views/SubscriptionNew.vue'),
-    meta: { requiresAuth: true }
-  },
-  {
-    path: '/node-groups/:id',
-    name: 'NodeGroupEdit',
-    component: () => import('../views/NodeGroupEdit.vue'),
-    meta: { requiresAuth: true }
-  },
-  {
-    path: '/node-groups/new',
-    name: 'NodeGroupNew',
-    component: () => import('../views/NodeGroupNew.vue'),
-    meta: { requiresAuth: true }
-  },
-  {
-    path: '/rule-sets/:id',
-    name: 'RuleSetEdit',
-    component: () => import('../views/RuleSetEdit.vue'),
-    meta: { requiresAuth: true }
-  },
-  {
-    path: '/rule-sets/new',
-    name: 'RuleSetNew',
-    component: () => import('../views/RuleSetNew.vue'),
-    meta: { requiresAuth: true }
   },
 ]
 
